3149f6ddff9a07a9b1fd9e62e3af6a7ea07ccaf1
Josh Vasquez <Joshvasquez204@gmail.com>
2019-11-06 00:48:08 -0600

    updated js

---
+++ client/build/serviceworker.js
@@ -0,0 +1,21 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.9915c4a8ac296bb24b1d8fa71aad2e7a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
